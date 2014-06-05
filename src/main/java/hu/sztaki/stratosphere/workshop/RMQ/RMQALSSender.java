d7a20aae0651a95d49c8b153915e5b7c409ab436
gyfora <gyula.fora@gmail.com>
2014-06-05 23:22:05 +0200

    source for random ids

---
+++ src/main/java/hu/sztaki/stratosphere/workshop/RMQ/RMQALSSender.java
@@ -0,0 +1,46 @@
+/***********************************************************************************************************************
+ *
+ * Copyright (C) 2010-2014 by the Stratosphere project (http://stratosphere.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ **********************************************************************************************************************/
+
+package hu.sztaki.stratosphere.workshop.RMQ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Random;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class RMQALSSender {
+	private final static String QUEUE_NAME = "hello";
+
+	public static void main(String[] argv) throws java.io.IOException, InterruptedException {
+
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+		Random rnd = new Random();
+		for (int i = 0; i < 1000; i++) {
+			Thread.sleep(10);
+			channel.basicPublish("", QUEUE_NAME, null, Integer.toString(rnd.nextInt(250))
+					.getBytes());
+		}
+		channel.close();
+		connection.close();
+	}
+}
