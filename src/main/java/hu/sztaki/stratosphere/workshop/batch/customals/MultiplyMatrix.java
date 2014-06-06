2e16704dd1269b45640f0ce2f56f467632702c8a
Till Rohrmann <till.rohrmann@gmail.com>
2014-06-06 05:51:05 +0200

    minor refactoring

--- src/main/java/hu/sztaki/stratosphere/workshop/batch/customals/MultiplyMatrix.java
+++ src/main/java/hu/sztaki/stratosphere/workshop/batch/customals/MultiplyMatrix.java
@@ -15,35 +15,28 @@
 
 package hu.sztaki.stratosphere.workshop.batch.customals;
 
-import java.util.Collection;
-import java.util.Iterator;
-
-import eu.stratosphere.util.Collector;
 import eu.stratosphere.api.java.functions.MapFunction;
 import eu.stratosphere.api.java.tuple.Tuple3;
 import eu.stratosphere.api.java.tuple.Tuple5;
 
 public class MultiplyMatrix extends MapFunction<Tuple3<Integer,Integer,Double>,Partition<MatrixEntry>> {
 
-  private int numOfTasks;
-  private Tuple5<Integer,Boolean,Integer,Integer,double[]> output = new Tuple5();
-  private int index;
-  
-  public MultiplyMatrix(int numTask, int index) {
-    this.numOfTasks = numTask; 
-    this.index = index;
-  }
-  
-  @Override
-  public Partition<MatrixEntry> map(Tuple3<Integer,Integer, Double> record) throws Exception {
+	private int numOfTasks;
+	private Tuple5<Integer,Boolean,Integer,Integer,double[]> output = new Tuple5();
+	private int index;
 
-    int ownIndex = record.getField(index);//when ==0 then we do rowwise partition, when ==1 we do a columnwise partition
-    
-    //TODO: assign each element of the (sparse) rating matrix uniformly to a machine
-    //TODO: the output vector has the (machinceIndex, TRUE, rowID, columnID, double[1]{elementValue}) format
-    
-    return null;
-  
-  }
+	public MultiplyMatrix(int numTask, int index) {
+		this.numOfTasks = numTask;
+		this.index = index;
+	}
+
+	@Override
+	public Partition<MatrixEntry> map(Tuple3<Integer,Integer, Double> record) throws Exception {
+		int ownIndex = record.getField(index);//when ==0 then we do rowwise partition, when ==1 we do a columnwise partition
+		//TODO: assign each element of the (sparse) rating matrix uniformly to a machine
+		//TODO: the output vector has the (machinceIndex, TRUE, rowID, columnID, double[1]{elementValue}) format
+
+		return null;
+	}
 
 }
